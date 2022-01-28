58ad584ec19515cfbc8a321d6202183f3c3bb124
david de la puente enriquez <Twinkymen@hotmail.com>
2022-01-28 12:35:42 +0100

    Documentación mejorada

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         int[] numeros = new int[6]; // Se declara variable num y se pone limite de 6.
         Scanner sc = new Scanner(System.in); //
+
+        //Bucle para rellenar el array con los números del usuario
         for (int i = 0; i < numeros.length; i++) {
             System.out.println("Introduce un numero: ");
             int numero = sc.nextInt();

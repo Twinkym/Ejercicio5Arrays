b71223c5c66f68ab373be14cf513cad54a009d26
david de la puente enriquez <Twinkymen@hotmail.com>
2022-01-28 12:25:36 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numeros = new int[6]; // Se declara variable num y se pone limite de 6.
+        Scanner sc = new Scanner(System.in); //
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Introduce un numero: ");
+            int numero = sc.nextInt();
+            numeros[i] = numero;
+        }
+        System.out.println(numeros);
+
+        for (int i = numeros.length-1; i >= 0; i--) {
+            System.out.println(numeros[i]);
+        }
+    }
+}
+
